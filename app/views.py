2953bf4904dd766ba123db9446289b411d5ed3e2
kasindalasunil <kasindalasunil316@gmail.com>
2023-11-09 21:25:24 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def macha(request):
+    return render(request,'macha.html')
+def balayya(request):
+    return render(request,'balayya.html')
+def prabhas(request):
+    return render(request,'prabhas.html')
+def harshad(request):
+    return render(request,'harshad.html')
